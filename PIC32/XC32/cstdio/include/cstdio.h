acab22f9dfdbb29ca55e1e63bcba0bcbba0b8ac7
Vladimir Cvetkovic <vladimir.cvetkovic@mikroe.com>
2024-05-31 12:56:14 +0200

    cstdio.h comments done

--- PIC32/XC32/cstdio/include/cstdio.h
+++ PIC32/XC32/cstdio/include/cstdio.h
@@ -9,18 +9,14 @@
 
   All rights reserved.
 
------------------------------------------------------------------------------ */
+---------------------------------------------------------------------------- */
 
 /**
-    \file   cstdio.h
-    \brief  Standard input output functions.
-    \{
-
-- Version : **1.0.0**
-- Date : **02.03.2023**
-- Developer : **MikroE Team**
-
+ * @file cstdio.h
+ * @brief Standard input output functions.
+ * @{
 */
+
 // ----------------------------------------------------------------------------
 
 #ifndef _CSTDIO_H_
@@ -36,7 +32,6 @@ extern "C"{
 /**
  * @brief MikroE standard output breakpoint.
  * @details This routine is used by built-in terminal.
- * @param / None
  * @return None
  */
 static inline void stdBuffFullBreak() {
@@ -45,8 +40,8 @@ static inline void stdBuffFullBreak() {
 
 /**
  * @brief Writes a character to stdout.
- * @details The C library function int putchar(int char) writes a character (an unsigned char)
- *          specified by the argument char to stdout.
+ * @details The C library function int putchar(int char) writes a character (an
+ *          unsigned char) specified by the argument char to stdout.
  * @param[in] character This is the character to be written.
  * @note @b character is passed as its int promotion.
  * @note MikroE standard output implementation.
@@ -57,8 +52,9 @@ int putchar_me( int character );
 
 /**
  * @brief Writes a string to stdout.
- * @details The C library function int puts(const char *str) writes a string to stdout up to
- *          but not including the null character. A newline character is appended to the output.
+ * @details The C library function int puts(const char *str) writes a string to
+ *          stdout up to but not including the null character. A newline
+ *          character is appended to the output.
  * @param[in] str This is the C string to be written.
  * @note MikroE standard output implementation.
  * @return None
@@ -67,11 +63,11 @@ int puts_me( const char *str );
 
 /**
  * @brief Writes a string to specified stream.
- * @details The C library function int fputs(const char *str, FILE *stream) writes a string
- *          to the specified stream up to but not including the null character.
- * @param[in] str This is the C string to be written.
- * @param[out] stream This is the pointer to a FILE object that identifies the stream
- *                    where the string is to be written.
+ * @details The C library function int fputs(const char *str, FILE *stream) writes a
+ *          string to the specified stream up to but not including the null character.
+ * @param[in]  str    This is the C string to be written.
+ * @param[out] stream This is the pointer to a FILE object that identifies the
+ *                    stream where the string is to be written.
  * @note MikroE standard output implementation.
  * @return None
  */
@@ -79,23 +75,26 @@ int fputs_me( const char *str, void (*stream)(char ch) );
 
 /**
  * @brief MikroE standard output API.
- * @details This routine is used by NECTO Studio
- *          to send log output to the built-in terminal.
+ * @details This routine is used by NECTO Studio to send log output to the
+ *          built-in terminal.
  * @param[in] ch String to be written.
  * @return None
  */
+
 void debugStdOut( char ch );
 
 /**
  * @brief Sends formatted output to stdout.
- * @param[in] format This is the string that contains the text to be written to stdout.
- *            It can optionally contain embedded format tags that are replaced by the values
- *            specified in subsequent additional arguments and formatted as requested.
- *            Format tags prototype is @b %[flags][width][.precision][length]specifier
- * @note Depending on the format string, the function may expect a sequence of additional arguments,
- *       each containing one value to be inserted instead of each @b %-tag specified in
- *       the format parameter (if any).
- *       There should be the same number of these arguments as the number of @b %-tags that expect a value.
+ * @param[in] format This is the string that contains the text to be written to
+ *                   stdout. It can optionally contain embedded format tags that
+ *                   are replaced by the values specified in subsequent additional
+ *                   arguments and formatted as requested. Format tags prototype
+ *                   is @b %[flags][width][.precision][length]specifier.
+ * @note Depending on the format string, the function may expect a sequence of
+ *       additional arguments, each containing one value to be inserted instead
+ *       of each @b %-tag specified in the format parameter (if any). There
+ *       should be the same number of these arguments as the number of @b %-tags
+ *       that expect a value.
  * @return If successful, the total number of characters written is returned.
  *         On failure, a negative number is returned.
  */
@@ -104,14 +103,16 @@ int printf_me( const char *format, ... );
 /**
  * @brief Sends formatted output to a stream.
  * @param[out] stream This is the pointer to an object that identifies the stream.
- * @param[in] format This is the string that contains the text to be written to stdout.
- *            It can optionally contain embedded format tags that are replaced by the values
- *            specified in subsequent additional arguments and formatted as requested.
- *            Format tags prototype is @b %[flags][width][.precision][length]specifier
- * @note Depending on the format string, the function may expect a sequence of additional arguments,
- *       each containing one value to be inserted instead of each @b %-tag specified in
- *       the format parameter (if any).
- *       There should be the same number of these arguments as the number of @b %-tags that expect a value.
+ * @param[in]  format This is the string that contains the text to be written to
+ *                    the stream. It can optionally contain embedded format tags
+ *                    that are replaced by the values specified in subsequent
+ *                    additional arguments and formatted as requested. Format
+ *                    tags prototype is @b %[flags][width][.precision][length]specifier.
+ * @note Depending on the format string, the function may expect a sequence of
+ *       additional arguments, each containing one value to be inserted instead
+ *       of each @b %-tag specified in the format parameter (if any). There
+ *       should be the same number of these arguments as the number of @b %-tags
+ *       that expect a value.
  * @return If successful, the total number of characters written is returned.
  *         On failure, a negative number is returned.
  */
@@ -120,16 +121,19 @@ int fprintf_me( void (*stream)(char ch), const char *format, ... );
 /**
  * @brief Sends formatted output to a stream using an argument list passed to it.
  * @param[out] stream This is the pointer to an object that identifies the stream.
- * @param[in] format This is the string that contains the text to be written to stdout.
- *            It can optionally contain embedded format tags that are replaced by the values
- *            specified in subsequent additional arguments and formatted as requested.
- *            Format tags prototype is @b %[flags][width][.precision][length]specifier
- * @param[in] arg An object representing the variable arguments list.
- *            This should be initialized by the @ref va_start macro defined in @b <stdarg>
- * @note Depending on the format string, the function may expect a sequence of additional arguments,
- *       each containing one value to be inserted instead of each @b %-tag specified in
- *       the format parameter (if any).
- *       There should be the same number of these arguments as the number of @b %-tags that expect a value.
+ * @param[in]  format This is the string that contains the text to be written to
+ *                    the stream. It can optionally contain embedded format tags
+ *                    that are replaced by the values specified in subsequent
+ *                    additional arguments and formatted as requested. Format
+ *                    tags prototype is @b %[flags][width][.precision][length]specifier.
+ * @param[in]  arg    An object representing the variable arguments list. This
+ *                    should be initialized by the @ref va_start macro defined
+ *                    in @b <stdarg>.
+ * @note Depending on the format string, the function may expect a sequence of
+ *       additional arguments, each containing one value to be inserted instead
+ *       of each @b %-tag specified in the format parameter (if any). There
+ *       should be the same number of these arguments as the number of @b %-tags
+ *       that expect a value.
  * @return If successful, the total number of characters written is returned.
  *         On failure, a negative number is returned.
  */
@@ -141,7 +145,7 @@ int vfprintf_me( void (*stream)(char ch), const char *format, register va_list a
 
 #endif  // _CSTDIO_H_
 
-/// \}
+/** @} */
 // ----------------------------------------------------------------------------
 /*
     cstdio.h
