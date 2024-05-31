741915be6581c721b5d1c667b17713367c7d3c42
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2024-05-31 15:18:59 +0200

    Formatted correct file and reverted q43

--- PIC/mikroC/system/P18/src/__Lib_System_Q43.c
+++ PIC/mikroC/system/P18/src/__Lib_System_Q43.c
@@ -1,119 +1,117 @@
-char __Low_saveWREG;
+//kopiranje iz cdata u (data v idata) prostore
+//dptr sadrzi adresu iz cdata, r0 sadrzi adresu iz (data,idata)
+//r1 broj bajtova koji treba prebaciti (max 255)
+
+char __Low_saveWREG;  // koriste se za spasavanje konteksa za low interrupt funckiju, moraju biti u access banci
 char __Low_saveSTATUS;
 char __Low_saveBSR;
 
-void __CC2D()
-{
-    asm {
-        _CC2D_Loop1:
-        TBLRD*+
-        MOVFF POSTINC1, TABLAT
-        DECF R0, 1
-        BNZ _CC2D_Loop1
+void __CC2D(){
+ asm{
+     _CC2D_Loop1:
+     TBLRD*+
+     MOVFF POSTINC1, TABLAT
+     DECF R0, 1
+     BNZ _CC2D_Loop1
     }
 }
 
-void __FillZeros()
-{
-    while ( FSR0 > FSR1 )
+void __FillZeros(){
+     while(FSR0 > FSR1)
         POSTINC1 = 0;
 }
 
-void __CC2DW()
-{
-    asm {
-        _CC2DL_Loop1:
-        TBLRD*+
-        MOVFF TABLAT, POSTINC1
-        DECF   R0, 1
-        BNZ _CC2DL_Loop1
-        DECF   R1, 1
-        BNZ _CC2DL_Loop1
+void __CC2DW(){
+ asm{
+     _CC2DL_Loop1:
+     TBLRD*+
+     MOVFF TABLAT, POSTINC1
+     DECF   R0, 1
+     BNZ _CC2DL_Loop1
+     DECF   R1, 1
+     BNZ _CC2DL_Loop1
     }
 }
 
-void __CA2AW()
-{
-    asm {
-        MOVLW      0
-        DECF       R0, 1
-        SUBWFB     R1, 1
-    L_loopCA2AWs:
-        MOVFF      POSTINC2, POSTINC1
-        DECF       R0, 1
-        SUBWFB     R1, 1
-        BC        L_loopCA2AWs
+void __CA2AW(){
+    asm{
+          MOVLW      0
+          DECF       R0, 1
+          SUBWFB     R1, 1
+      L_loopCA2AWs:
+          MOVFF      POSTINC2, POSTINC1
+          DECF       R0, 1
+          SUBWFB     R1, 1
+          BC        L_loopCA2AWs
     }
 }
 
-void __CCA2AW()
-{
-    asm {
-        MOVLW      0
-        DECF       R0, 1
-        SUBWFB     R1, 1
-    L_loopCCA2AWs:
-        TBLRD*+
-        MOVFF      TABLAT, POSTINC1
-        DECF       R0, 1
-        SUBWFB     R1, 1
-        BC        L_loopCCA2AWs
+void __CCA2AW(){
+    asm{
+          MOVLW      0
+          DECF       R0, 1
+          SUBWFB     R1, 1
+      L_loopCCA2AWs:
+          TBLRD*+
+          MOVFF      TABLAT, POSTINC1
+          DECF       R0, 1
+          SUBWFB     R1, 1
+          BC        L_loopCCA2AWs
     }
 }
 
-void __CS2S()
-{
-    asm {
-    L_loopCS2Ss:
-        MOVF       POSTINC2, 0
-        BZ         L_CS2Send
-        MOVWF      POSTINC1
-        GOTO       L_loopCS2Ss
-    L_CS2Send:
+void __CS2S(){
+    asm{
+      L_loopCS2Ss:
+          MOVF       POSTINC2, 0
+          BZ         L_CS2Send
+          MOVWF      POSTINC1
+          GOTO       L_loopCS2Ss
+      L_CS2Send:
     }
 }
 
-void __CCS2S()
-{
-    asm {
-    L_loopCCS2Ss:
-        TBLRD*+
-        MOVF       TABLAT, 0
-        BZ         L_CCS2Send
-        MOVWF      POSTINC1
-        GOTO       L_loopCCS2Ss
-    L_CCS2Send:
+void __CCS2S(){
+    asm{
+      L_loopCCS2Ss:
+          TBLRD*+
+          MOVF       TABLAT, 0
+          BZ         L_CCS2Send
+          MOVWF      POSTINC1
+          GOTO       L_loopCCS2Ss
+      L_CCS2Send:
     }
 }
 
-void __FZinS()
-{
-    asm {
-    L_loopFZinSWs:
-        TSTFSZ     POSTINC1
-        GOTO       L_loopFZinSWs
-        MOVF       POSTDEC1, 0
+void __FZinS(){
+    asm{
+      L_loopFZinSWs:
+          TSTFSZ     POSTINC1
+          GOTO       L_loopFZinSWs
+          MOVF       POSTDEC1, 0
     }
 }
 
-void ____DoIFC()
-{
-    asm MOVF  INTCON0, 0;
-    asm MOVWF R2;
-    INTCON0.GIE = 0;
-    asm push;
-    asm CLRF TOSU;
-    asm MOVF R1, 0 asm MOVWF TOSH;
-    asm MOVF R0, 0 asm MOVWF TOSL;
-    asm BTFSC R2, GIE;
-    asm BSF   INTCON0, GIE;
-    return;
+void ____DoIFC(){
+  asm MOVF INTCON0,0;
+  asm MOVWF R2;                              // spasavanje GIE(INTCON0-a) mora da ide preko  R2 da bi contex saving sprecio interrupt , ne sme da se pamti ceo INTCON0 jer moze korisnik da ga menja
+  INTCON0.GIE = 0;                            // disable interrupts
+  asm push;                                  // menjanje i petljanje
+  asm CLRF TOSU;                            // TOS-a
+  asm MOVF R1,0
+  asm MOVWF TOSH;
+  asm MOVF R0,0
+  asm MOVWF TOSL;
+  asm BTFSC R2,GIE;
+  asm BSF INTCON0,GIE;
+  return;                                  // sad pozovi funkciju tako sto return odradi
+
 }
 
-char Swap( char input )
-{
-    R0 = input;
-    asm swapf R0, F return R0;
+char Swap(char input) {
+  R0 = input;
+  asm swapf R0, F
+  return R0;
 }
 
 /******************************************************************************/
@@ -150,25 +148,24 @@ char Swap( char input )
 /*    0.00 | Created function setjmp                          | 231106 | ST   */
 /*         |                                                  |        |      */
 /******************************************************************************/
-int setjmp( jmp_buf env )
-{
-    env[ 3 ] = STKPTR;
-    asm MOVF  INTCON0, 0;
-    asm MOVWF R2;
-    INTCON0.GIE = 0;
-    asm {
-        MOVFF        FARG_setjmp_env+0, FSR1L
-        MOVFF        FARG_setjmp_env+1, FSR1H
-        MOVF        TOSU, W
-        MOVWF       POSTINC1
-        MOVF        TOSH, W
-        MOVWF       POSTINC1
-        MOVF        TOSL, W
-        MOVWF       INDF1
-    }
-    asm BTFSC R2, GIE;
-    asm BSF   INTCON0, GIE;
-    return 0;
+int setjmp(jmp_buf env){
+  env[3] = STKPTR;
+  asm  MOVF INTCON0,0;
+  asm  MOVWF R2;                              // spasavanje GIE(INTCON0-a) mora da ide preko  R2 da bi contex saving sprecio interrupt , ne sme da se pamti ceo INTCON0 jer moze korisnik da ga menja
+  INTCON0.GIE = 0;                            // disable interrupts
+  asm {
+    MOVFF        FARG_setjmp_env+0, FSR1L
+    MOVFF        FARG_setjmp_env+1, FSR1H
+    MOVF        TOSU, W
+    MOVWF       POSTINC1
+    MOVF        TOSH, W
+    MOVWF       POSTINC1
+    MOVF        TOSL, W
+    MOVWF       INDF1
+  }
+  asm BTFSC R2,GIE;
+  asm BSF INTCON0,GIE;
+  return 0;
 }
 
 /******************************************************************************/
@@ -191,25 +188,24 @@ int setjmp( jmp_buf env )
 /*    0.00 | Created function                                 | 231106 | ST   */
 /*         |                                                  |        |      */
 /******************************************************************************/
-void longjmp( jmp_buf env, int val )
-{
-    if ( !val )
-        val = 1;
-    asm MOVF  INTCON0, 0;
-    asm MOVWF R2;
-    INTCON0.GIE = 0;
-    STKPTR      = env[ 3 ];
-    asm {
-        MOVFF       FARG_longjmp_env+0, FSR0L
-        MOVFF       FARG_longjmp_env+1, FSR0H
-        MOVF        POSTINC0, W
-        MOVWF       TOSU
-        MOVF        POSTINC0, W
-        MOVWF       TOSH
-        MOVF        INDF0, W
-        MOVWF       TOSL
-        MOVFF       FARG_longjmp_env+2, R0
-    }
-    asm BTFSC R2, GIE;
-    asm BSF   INTCON0, GIE;
+void longjmp(jmp_buf  env, int val){
+  if (!val)
+    val = 1;
+  asm  MOVF INTCON0,0;
+  asm  MOVWF R2;                              // spasavanje GIE(INTCON0-a) mora da ide preko  R2 da bi contex saving sprecio interrupt , ne sme da se pamti ceo INTCON0 jer moze korisnik da ga menja
+  INTCON0.GIE = 0;                            // disable interrupts
+  STKPTR = env[3];
+  asm {
+    MOVFF       FARG_longjmp_env+0, FSR0L
+    MOVFF       FARG_longjmp_env+1, FSR0H
+    MOVF        POSTINC0, W
+    MOVWF       TOSU
+    MOVF        POSTINC0, W
+    MOVWF       TOSH
+    MOVF        INDF0, W
+    MOVWF       TOSL
+    MOVFF       FARG_longjmp_env+2, R0
+  }
+  asm BTFSC R2,GIE;
+  asm BSF INTCON0,GIE;
 }
