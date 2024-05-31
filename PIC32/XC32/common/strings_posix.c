2e363c10296cd8cf1501ce3aea9efb752b6387e1
Vladimir Cvetkovic <vladimir.cvetkovic@mikroe.com>
2024-05-31 12:37:32 +0200

    string_posix.c all done

--- PIC32/XC32/common/strings_posix.c
+++ PIC32/XC32/common/strings_posix.c
@@ -1,46 +1,56 @@
-// TODO This is temorary soultion for XC32 built in functions not linking problem. Must be deleted later.
+// TODO Temporary solution for XC32 built-in functions not linking. Delete when fixed.
 #include <sys/types.h>
 
-int ffs(int i) {
+int ffs( int i )
+{
     static const unsigned char table[] = {
-        0,1,2,2,3,3,3,3,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,
-        6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,
-        7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-        7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-        8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,
-        8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,
-        8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,
-        8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8
+        0, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4,
+        5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+        6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+        6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+        7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+        7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+        7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+        7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+        8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+        8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+        8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+        8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+        8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+        8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+        8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+        8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8
     };
     unsigned int a;
     unsigned int x = i & -i;
-    a = x <= 0xffff ? (x <= 0xff ? 0 : 8) : (x <= 0xffffff ?  16 : 24);
-    return table[x >> a] + a;
+    a = x <= 0xffff ? ( x <= 0xff ? 0 : 8 ) : ( x <= 0xffffff ? 16 : 24 );
+    return table[ x >> a ] + a;
 }
 
-int strncasecmp(const char *s1, const char *s2, size_t n)
+int strncasecmp( const char * s1, const char * s2, size_t n )
 {
-    if (n == 0)
+    if ( n == 0 )
         return 0;
 
-    while (n-- != 0 && tolower(*s1) == tolower(*s2)) {
-        if (n == 0 || *s1 == '\0' || *s2 == '\0')
+    while ( n-- != 0 && tolower( *s1 ) == tolower( *s2 ) ) {
+        if ( n == 0 || *s1 == '\0' || *s2 == '\0' )
             break;
         s1++;
         s2++;
     }
 
-    return tolower(*(unsigned char *) s1) - tolower(*(unsigned char *) s2);
+    return tolower( *( unsigned char * )s1 ) - tolower( *( unsigned char * )s2 );
 }
 
-int strcasecmp(const char *s1, const char *s2) {
-    const unsigned char *p1 = (const unsigned char *) s1;
-    const unsigned char *p2 = (const unsigned char *) s2;
-    int result;
-    if (p1 == p2)
+int strcasecmp( const char * s1, const char * s2 )
+{
+    const unsigned char * p1 = ( const unsigned char * )s1;
+    const unsigned char * p2 = ( const unsigned char * )s2;
+    int                   result;
+    if ( p1 == p2 )
         return 0;
-    while ((result = tolower(*p1) - tolower(*p2++)) == 0)
-        if (*p1++ == '\0')
+    while ( ( result = tolower( *p1 ) - tolower( *p2++ ) ) == 0 )
+        if ( *p1++ == '\0' )
             break;
     return result;
 }
