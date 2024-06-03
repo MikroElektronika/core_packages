9502d3919004a5312dfe586a9de0cee670977e8c
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2024-06-03 15:35:30 +0200

    fixed tabs

--- ARM/mikroC/system/ST/M0/include/RCC_typedef.h
+++ ARM/mikroC/system/ST/M0/include/RCC_typedef.h
@@ -2,8 +2,8 @@
 
 typedef struct
 {
-  unsigned long SYSCLK_Frequency; // SYSCLK clock frequency in Hz
-  unsigned long HCLK_Frequency;   // HCLK clock frequency   in Hz
-  unsigned long PCLK_Frequency;  // PCLK1 clock frequency  in Hz
-  unsigned long ADCCLK_Frequency; // ADCCLK clock frequency in Hz
+    unsigned long SYSCLK_Frequency; // SYSCLK clock frequency in Hz
+    unsigned long HCLK_Frequency;   // HCLK clock frequency   in Hz
+    unsigned long PCLK_Frequency;  // PCLK1 clock frequency  in Hz
+    unsigned long ADCCLK_Frequency; // ADCCLK clock frequency in Hz
 } RCC_ClocksTypeDef;
--- ARM/mikroC/system/ST/M0/include/RCC_typedef_M0Plus.h
+++ ARM/mikroC/system/ST/M0/include/RCC_typedef_M0Plus.h
@@ -2,9 +2,8 @@
 
 typedef struct
 {
-  unsigned long SYSCLK_Frequency; // SYSCLK clock frequency in Hz
-  unsigned long HCLK_Frequency;   // HCLK clock frequency   in Hz
-  unsigned long PCLK1_Frequency;  // PCLK1 clock frequency  in Hz
-  unsigned long PCLK2_Frequency;  // PCLK2 clock frequency  in Hz
-//  unsigned long ADCCLK_Frequency; // ADCCLK clock frequency in Hz
+    unsigned long SYSCLK_Frequency; // SYSCLK clock frequency in Hz
+    unsigned long HCLK_Frequency;   // HCLK clock frequency   in Hz
+    unsigned long PCLK1_Frequency;  // PCLK1 clock frequency  in Hz
+    unsigned long PCLK2_Frequency;  // PCLK2 clock frequency  in Hz
 } RCC_ClocksTypeDef;
