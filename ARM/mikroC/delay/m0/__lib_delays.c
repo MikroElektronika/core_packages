9c39eed55c77057fe091a3b49f6af9ffddd2be08
Vladimir Cvetkovic <vladimir.cvetkovic@mikroe.com>
2024-06-03 09:10:46 +0200

    added missing comment heading to __lib_delays.c files

--- ARM/mikroC/delay/m0/__lib_delays.c
+++ ARM/mikroC/delay/m0/__lib_delays.c
@@ -1,3 +1,16 @@
+/*
+    __lib_delays.c
+
+ ------------------------------------------------------------------------------
+
+  This file is part of mikroSDK.
+
+  Copyright (c) 2024, MikroElektonika - www.mikroe.com
+
+  All rights reserved.
+
+---------------------------------------------------------------------------- */
+
 #pragma setintsize mE
 
 unsigned long __System_CLOCK_IN_KHZ;
@@ -221,3 +234,30 @@ void VDelay_Advanced_ms( unsigned long Time_ms, unsigned long Current_Fosc_kHz )
 
     Delay_Cyc_16( ( NumberOfCyc >> 4 ) - 1 );
 }
+
+// ----------------------------------------------------------------------------
+/*
+    __lib_delays.c
+
+    Copyright (c) 2024, MikroElektronika - www.mikroe.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+// ----------------------------------------------------------------------------
