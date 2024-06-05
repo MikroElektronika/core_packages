e6003e7b204154856ac10c925de9f29ee0929ae2
Vladimir Cvetkovic <vladimir.cvetkovic@mikroe.com>
2024-06-05 10:24:45 +0200

    all headers formatted

--- ARM/gcc_clang/system/inc/stm/system_stm32g_0xx.h
+++ ARM/gcc_clang/system/inc/stm/system_stm32g_0xx.h
@@ -2,8 +2,8 @@
 
 typedef struct
 {
-  unsigned long SYSCLK_Frequency; // SYSCLK clock frequency  in Hz
-  unsigned long HCLK_Frequency;   // HCLK   clock frequency  in Hz
-  unsigned long PCLK_Frequency;   // PCLK   clock frequency  in Hz
-  unsigned long TPCLK_Frequency;  // TPCLK  clock frequency  in Hz
+    unsigned long SYSCLK_Frequency; // SYSCLK clock frequency  in Hz
+    unsigned long HCLK_Frequency;   // HCLK   clock frequency  in Hz
+    unsigned long PCLK_Frequency;   // PCLK   clock frequency  in Hz
+    unsigned long TPCLK_Frequency;  // TPCLK  clock frequency  in Hz
 } RCC_ClocksTypeDef;
