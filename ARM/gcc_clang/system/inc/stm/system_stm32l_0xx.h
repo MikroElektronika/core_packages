8ee2ffd5c04e7db91518070c5c703b14a141f703
Vladimir Cvetkovic <vladimir.cvetkovic@mikroe.com>
2024-06-05 10:28:49 +0200

    removed comment from a header

--- ARM/gcc_clang/system/inc/stm/system_stm32l_0xx.h
+++ ARM/gcc_clang/system/inc/stm/system_stm32l_0xx.h
@@ -6,5 +6,4 @@ typedef struct
     unsigned long HCLK_Frequency;   // HCLK clock frequency   in Hz
     unsigned long PCLK1_Frequency;  // PCLK1 clock frequency  in Hz
     unsigned long PCLK2_Frequency;  // PCLK2 clock frequency  in Hz
-    //  unsigned long ADCCLK_Frequency; // ADCCLK clock frequency in Hz
 } RCC_ClocksTypeDef;
