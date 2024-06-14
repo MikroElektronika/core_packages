edc86e02a4eaeb14617859e97b0c0c71687e4c96
Stefan Djordjevic <stefan.djordjevic@mikroe.com>
2024-06-14 09:23:35 +0200

    merged sam20 and sam21 into 1 system as it is in datasheet

--- ARM/gcc_clang/cmake/sam/atsamc20.cmake
+++
@@ -1,7 +0,0 @@
-if(${MCU_NAME} MATCHES "^ATSAMC20E15A$|^ATSAMC20E16A$|^ATSAMC20E17A$|^ATSAMC20E18A$|^ATSAMC20G15A$|^ATSAMC20G16A$|^ATSAMC20G17A$|^ATSAMC20G18A$|^ATSAMC20J15A$|^ATSAMC20J16A$|^ATSAMC20J17A$|^ATSAMC20J18A$|^ATSAMC20N17A$|^ATSAMC20N18A")
-    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
-    set(${startupFile} startup/${vendor}/${mcu_match}.c PARENT_SCOPE)
-    list(APPEND local_list_include system/src/sam/system_samc20.c)
-    list(APPEND local_list_include common/syscalls.c)
-endif()
-
