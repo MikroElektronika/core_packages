725b28b4e81199031195140b25e638efd9bbd425
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2024-10-30 10:11:46 +0100

    Added release hw to new-feature/mcus/40

--- ARM/gcc_clang/cmake/stm/delays/stm32u575qg_i.cmake
+++ ARM/gcc_clang/cmake/stm/delays/stm32u575qg_i.cmake
@@ -1,3 +1,3 @@
 if(${MCU_NAME} MATCHES "^STM32U575QG$|^STM32U575QI$")
-    list(APPEND local_list_macros "getClockValue(_clock) (_clock/1000UL/17)")
+    list(APPEND local_list_macros "getClockValue(_clock) (_clock/1000/17)")
 endif()
