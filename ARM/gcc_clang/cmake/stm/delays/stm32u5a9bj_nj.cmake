ee1895e0a2eef8812c4bb18e88f5bbe5aedc54ba
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2024-10-30 10:13:19 +0100

    Added release hw to new-feature/mcus/47

--- ARM/gcc_clang/cmake/stm/delays/stm32u5a9bj_nj.cmake
+++ ARM/gcc_clang/cmake/stm/delays/stm32u5a9bj_nj.cmake
@@ -1,3 +1,3 @@
 if(${MCU_NAME} MATCHES "^STM32U5A9BJ$|^STM32U5A9NJ$")
-    list(APPEND local_list_macros "getClockValue(_clock) (_clock/1000UL/17)")
+    list(APPEND local_list_macros "getClockValue(_clock) (_clock/1000/17)")
 endif()
