e1f2fb4d1235e2ab2787ed83a7ceb0aaa1fb6de4
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2024-10-30 10:08:48 +0100

    Added release hw to new-feature/mcus/26

--- ARM/gcc_clang/cmake/stm/delays/stm32h563ag_i.cmake
+++ ARM/gcc_clang/cmake/stm/delays/stm32h563ag_i.cmake
@@ -1,3 +1,3 @@
 if(${MCU_NAME} MATCHES "^STM32H563AG$|^STM32H563AI$")
-    list(APPEND local_list_macros "getClockValue(_clock) (_clock/1000UL/17)")
+    list(APPEND local_list_macros "getClockValue(_clock) (_clock/1000/17)")
 endif()
