d7e597bfa0e93097f19e3d0d4940db2dacf343c7
DaniloMilenkovic <danilo.milenkovic@mikroe.com>
2024-11-20 09:15:49 +0100

    Update release_calendar.py

--- scripts/release_calendar.py
+++ scripts/release_calendar.py
@@ -118,7 +118,7 @@ def find_branch():
 
     for release in json_data['NECTO DAILY UPDATE']["events"]:
         date = release["start_dt"]
-        if "2025-02-05" == date:
+        if "2025-02-06" == date:
             return release["branch"]
 
     return "NO_BRANCH_IN_SPREADSHEET"
