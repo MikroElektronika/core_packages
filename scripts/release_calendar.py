a7c21bff20e826157089839b0a0c2983ba2ff331
Stevan Zecic <stevan.zecic@mikroe.com>
2024-11-27 12:34:04 +0100

    update for MK20DX128VMP5

--- scripts/release_calendar.py
+++ scripts/release_calendar.py
@@ -119,7 +119,7 @@ def find_branch():
 
     for release in json_data['NECTO DAILY UPDATE']["events"]:
         date = release["start_dt"]
-        if "2025-02-26" == date:
+        if "2025-02-27" == date:
             return release["branch"]
 
     return "NO_BRANCH_IN_SPREADSHEET"
@@ -131,7 +131,7 @@ def find_reference_manual():
 
     for release in json_data['NECTO DAILY UPDATE']["events"]:
         date = release["start_dt"]
-        if "2025-02-26" == date:
+        if "2025-02-27" == date:
             return json.loads(release["additional"].replace('""','"').replace('"{','{').replace('}"','}'))["pdf_link"].replace('.pdf', '')
 
     return "none"
