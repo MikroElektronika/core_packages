90b4bf04a8e1f0f1034fa1242275c63b2f190bcd
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-03-04 08:03:47 +0100

    Added release hw to new-feature/mcus/115

---
+++ ARM/gcc_clang/cmake/nxp/mks20fn_vft_vlh_vll_12.cmake
@@ -0,0 +1,5 @@
+if(${MCU_NAME} MATCHES "^MKS20FN128VFT12$|^MKS20FN256VFT12$|^MKS20FN128VLH12$|^MKS20FN256VLH12$|^MKS20FN128VLL12$|^MKS20FN256VLL12$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.s PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nxp/doc_ds_58/init_clock.c)
+endif()
