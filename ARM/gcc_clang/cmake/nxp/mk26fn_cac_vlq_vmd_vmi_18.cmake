a733c555a7154e7c3f23e4214b880ea82f8efa1a
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-03-04 08:03:36 +0100

    Added release hw to new-feature/mcus/114

---
+++ ARM/gcc_clang/cmake/nxp/mk26fn_cac_vlq_vmd_vmi_18.cmake
@@ -0,0 +1,5 @@
+if(${MCU_NAME} MATCHES "^MK26FN2M0VMD18$|^MK26FN2M0VMI18$|^MK26FN2M0VLQ18$|^MK26FN2M0CAC18R$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.s PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nxp/doc_ds_17/init_clock.c)
+endif()
