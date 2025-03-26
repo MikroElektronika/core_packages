b793d0fabcfd84d53e5742354bc08a4e90363666
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-03-26 12:08:38 +0100

    Added release hw to new-feature/mcus/129

---
+++ ARM/gcc_clang/cmake/nxp/mkl34z.cmake
@@ -0,0 +1,5 @@
+if(${MCU_NAME} MATCHES "^MKL34Z64VLH4$|^MKL34Z64VLL4$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.s PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nxp/mkl34z/init_clock.c)
+endif()
