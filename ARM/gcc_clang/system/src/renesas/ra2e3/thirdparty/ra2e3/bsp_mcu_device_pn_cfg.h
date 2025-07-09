02fc84e1925d5d3689d73f5471a2579ec1e9ee42
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-07-09 08:19:52 +0200

    Added release hw to new-feature/mcus/203

---
+++ ARM/gcc_clang/system/src/renesas/ra2e3/thirdparty/ra2e3/bsp_mcu_device_pn_cfg.h
@@ -0,0 +1,11 @@
+/* generated configuration header file - do not edit */
+#ifndef BSP_MCU_DEVICE_PN_CFG_H_
+#define BSP_MCU_DEVICE_PN_CFG_H_
+#define BSP_MCU_R7FA2E3073CNH
+      #define BSP_MCU_FEATURE_SET ('0')
+      #define BSP_ROM_SIZE_BYTES (65536)
+      #define BSP_RAM_SIZE_BYTES (16384)
+      #define BSP_DATA_FLASH_SIZE_BYTES (2048)
+      #define BSP_PACKAGE_QFN
+      #define BSP_PACKAGE_PINS (32)
+#endif /* BSP_MCU_DEVICE_PN_CFG_H_ */
