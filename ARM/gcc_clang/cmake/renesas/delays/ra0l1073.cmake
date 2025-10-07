d39f3a5c6794bd29a8111ce25ba2e873217c8711
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-10-07 11:51:50 +0200

    Added release hw to new-feature/mcus/1311

---
+++ ARM/gcc_clang/cmake/renesas/delays/ra0l1073.cmake
@@ -0,0 +1,3 @@
+if(${MCU_NAME} MATCHES "^R7FA0L1073CFJ$|^R7FA0L1073CFL$|^R7FA0L1073CNE$|^R7FA0L1073CNH$|^R7FA0L1073CNK$|^R7FA0L1073CSC$")
+    list(APPEND local_list_macros "getClockValue(_clock) (_clock/1000UL/5)")
+endif()
---
+++ ARM/gcc_clang/interrupts/include/interrupts_mcu/r7fa2t1074cfl/interrupts_mcu.h
@@ -0,0 +1,91 @@
+/****************************************************************************
+**
+** Copyright (C) 2025 MikroElektronika d.o.o.
+** Contact: https://www.mikroe.com/contact
+**
+** This file is part of the mikroSDK package
+**
+** Commercial License Usage
+**
+** Licensees holding valid commercial NECTO compilers AI licenses may use this
+** file in accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The MikroElektronika Company.
+** For licensing terms and conditions see
+** https://www.mikroe.com/legal/software-license-agreement.
+** For further information use the contact form at
+** https://www.mikroe.com/contact.
+**
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used for
+** non-commercial projects under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation: https://www.gnu.org/licenses/lgpl-3.0.html.
+**
+** The above copyright notice and this permission notice shall be
+** included in all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+** OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+** DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+** OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+** OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**
+****************************************************************************/
+/*!
+* @file  interrupts_mcu.h
+* @brief R7FA2T1074CFL MCU specific interrupt per module definitions.
+*/
+
+#ifndef _INTERRUPTS_MCU_H_
+#define _INTERRUPTS_MCU_H_
+
+// Interrupt table
+static const int INTERRUPTS_IELSR0 = 16;
+static const int INTERRUPTS_IELSR1 = 17;
+static const int INTERRUPTS_IELSR2 = 18;
+static const int INTERRUPTS_IELSR3 = 19;
+static const int INTERRUPTS_IELSR4 = 20;
+static const int INTERRUPTS_IELSR5 = 21;
+static const int INTERRUPTS_IELSR6 = 22;
+static const int INTERRUPTS_IELSR7 = 23;
+static const int INTERRUPTS_IELSR8 = 24;
+static const int INTERRUPTS_IELSR9 = 25;
+static const int INTERRUPTS_IELSR10 = 26;
+static const int INTERRUPTS_IELSR11 = 27;
+static const int INTERRUPTS_IELSR12 = 28;
+static const int INTERRUPTS_IELSR13 = 29;
+static const int INTERRUPTS_IELSR14 = 30;
+static const int INTERRUPTS_IELSR15 = 31;
+static const int INTERRUPTS_IELSR16 = 32;
+static const int INTERRUPTS_IELSR17 = 33;
+static const int INTERRUPTS_IELSR18 = 34;
+static const int INTERRUPTS_IELSR19 = 35;
+static const int INTERRUPTS_IELSR20 = 36;
+static const int INTERRUPTS_IELSR21 = 37;
+static const int INTERRUPTS_IELSR22 = 38;
+static const int INTERRUPTS_IELSR23 = 39;
+static const int INTERRUPTS_IELSR24 = 40;
+static const int INTERRUPTS_IELSR25 = 41;
+static const int INTERRUPTS_IELSR26 = 42;
+static const int INTERRUPTS_IELSR27 = 43;
+static const int INTERRUPTS_IELSR28 = 44;
+static const int INTERRUPTS_IELSR29 = 45;
+static const int INTERRUPTS_IELSR30 = 46;
+static const int INTERRUPTS_IELSR31 = 47;
+// EOF Interrupt table
+
+// Interrupt addresses
+// No interrupt registers for R7FA2T1074CFL.
+// EOF Interrupt addresses
+
+// Interrupt register bit values
+// No interrupt bits for R7FA2T1074CFL.
+// EOF Interrupt register bit values
+
+#endif // _INTERRUPTS_MCU_H_
+// ------------------------------------------------------------------------- END
