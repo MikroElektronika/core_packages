30960e41937d727bc283cdef9f646731f987b1bd
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-10-29 14:53:13 +0100

    Added release hw to new-feature/mcus/2611

---
+++ ARM/gcc_clang/cmake/infineon/cy9af4.cmake
@@ -0,0 +1,7 @@
+if(${MCU_NAME} MATCHES "^CY9AF421KPMC1-G-JNE2$|^CY9AF421KWQN-G-JNE2$|^CY9AF421LPMC-G-JNE2$|^CY9AF421LPMC1-G-JNE2$|^CY9AF421LWQN-G-JNE2$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/${vendor}/cy9af4/init_clock.c)
+    list(APPEND local_dir_install system/src/${vendor}/cy9af4/thirdparty/cy9af4)
+    set(${thirdpartyInstall} cy9af4/thirdparty/cy9af4 PARENT_SCOPE)
+endif()
---
+++ ARM/gcc_clang/cmake/infineon/delays/cy9af4.cmake
@@ -0,0 +1,3 @@
+if(${MCU_NAME} MATCHES "^CY9AF421KPMC1-G-JNE2$|^CY9AF421KWQN-G-JNE2$|^CY9AF421LPMC-G-JNE2$|^CY9AF421LPMC1-G-JNE2$|^CY9AF421LWQN-G-JNE2$")
+    list(APPEND local_list_macros "getClockValue(_clock) (_clock/1000UL/6)")
+endif()
