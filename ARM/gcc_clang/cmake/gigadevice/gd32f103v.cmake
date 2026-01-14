065f407da4ac0510ab0e37f474e35a1015693013
IvanRuzavin <ivan.ruzavin@mikroe.com>
2026-01-14 14:24:52 +0100

    Added release hw to new-feature/mcus/1602

---
+++ ARM/gcc_clang/cmake/gigadevice/gd32f103v.cmake
@@ -0,0 +1,5 @@
+if(${MCU_NAME} MATCHES "^GD32F103V8T6$|^GD32F103VBT6$|^GD32F103VCT6$|^GD32F103VDT6$|^GD32F103VET6$|^GD32F103VFT6$|^GD32F103VGT6$|^GD32F103VIT6$|^GD32F103VKT6$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/${vendor}/gd32f103v/init_clock.c)
+endif()
