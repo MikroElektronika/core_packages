3ebd5d04f7ea7cb71b67466ecb370013d4ea7ff0
IvanRuzavin <ivan.ruzavin@mikroe.com>
2026-02-10 15:12:57 +0100

    added missing semicolons for RA8M1 system file

--- ARM/gcc_clang/system/src/renesas/ra8m1/init_clock.c
+++ ARM/gcc_clang/system/src/renesas/ra8m1/init_clock.c
@@ -754,13 +754,13 @@ void SYSTEM_GetClocksFrequency( SYSTEM_ClocksTypeDef * SYSTEM_Clocks ) {
     SYSTEM_Clocks->SPICLK_Frequency = \
         SYSTEM_GetSPISCIClocksFrequency( VALUE_SYSTEM_SPICKCR & R_SYSTEM_SPICKCR_CKSEL_Msk );
     // Adjust SPICLK based on the SPICKDIVCR value.
-    SYSTEM_Clocks->SPICLK_Frequency /= SCI_SPI_CLK_PrescTable[ VALUE_SYSTEM_SPICKDIVCR & R_SYSTEM_SPICKDIVCR_CKDIV_Msk ]
+    SYSTEM_Clocks->SPICLK_Frequency /= SCI_SPI_CLK_PrescTable[ VALUE_SYSTEM_SPICKDIVCR & R_SYSTEM_SPICKDIVCR_CKDIV_Msk ];
 
     // Get the source clock of SCI module.
     SYSTEM_Clocks->SCICLK_Frequency = \
         SYSTEM_GetSPISCIClocksFrequency( VALUE_SYSTEM_SCICKCR & R_SYSTEM_SCICKCR_SCICKSEL_Msk );
     // Adjust SCICLK based on the SPICKDIVCR value.
-    SYSTEM_Clocks->SCICLK_Frequency /= SCI_SPI_CLK_PrescTable[ VALUE_SYSTEM_SCICKDIVCR & R_SYSTEM_SCICKDIVCR_CKDIV_Msk ]
+    SYSTEM_Clocks->SCICLK_Frequency /= SCI_SPI_CLK_PrescTable[ VALUE_SYSTEM_SCICKDIVCR & R_SYSTEM_SCICKDIVCR_CKDIV_Msk ];
 }
 
 /**
