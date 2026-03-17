c345ec68fa0c69978f4a21006e0b380c161749b4
IvanRuzavin <ivan.ruzavin@mikroe.com>
2026-03-17 10:28:11 +0100

    Added system library support for newly released MCUs

--- ARM/gcc_clang/cmake/renesas/delays/ra6m5_cag.cmake
+++ ARM/gcc_clang/cmake/renesas/delays/ra6m5_cag.cmake
@@ -1,3 +1,3 @@
 if(${MCU_NAME} MATCHES "^R7FA6M5BF3CAG$|^R7FA6M5BG3CAG$|^R7FA6M5BH3CAG$")
-    list(APPEND local_list_macros "getClockValue(_clock) (_clock/1000UL/4)")
+    list(APPEND local_list_macros "getClockValue(_clock) (_clock/1000UL/9)")
 endif()
