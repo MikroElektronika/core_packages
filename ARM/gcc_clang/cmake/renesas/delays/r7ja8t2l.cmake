c9521c0cf32bf33445a59ad72b1326483ddc993f
IvanRuzavin <ivan.ruzavin@mikroe.com>
2026-03-19 15:53:45 +0100

    Initial commit for RA8T2 system library support

--- ARM/gcc_clang/cmake/renesas/delays/r7ja8t2l.cmake
+++ ARM/gcc_clang/cmake/renesas/delays/r7ja8t2l.cmake
@@ -2,6 +2,6 @@ if(${MCU_NAME} MATCHES "^R7JA8T2LRDSAJ$|^R7JA8T2LRLSAJ$|^R7JA8T2LSDSAJ$|^R7JA8T2
     if(${CORE_NAME} MATCHES "^M85")
         list(APPEND local_list_macros "getClockValue(_clock) (_clock/1000UL/2)")
     elseif(${CORE_NAME} MATCHES "^M33EF")
-        list(APPEND local_list_macros "getClockValue(_clock) (_clock/1000UL/6)")
+        list(APPEND local_list_macros "getClockValue(_clock) (_clock/1000UL/3)")
     endif()
 endif()
