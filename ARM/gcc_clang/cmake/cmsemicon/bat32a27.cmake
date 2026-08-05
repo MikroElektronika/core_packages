182130644492c9a374d36674d917249cac91844a
IvanRuzavin <ivan.ruzavin@mikroe.com>
2026-08-05 15:54:47 +0200

    Added release hw to new-feature/mcus/2812

---
+++ ARM/gcc_clang/cmake/cmsemicon/bat32a27.cmake
@@ -0,0 +1,5 @@
+if(${MCU_NAME} MATCHES "^BAT32A279KM100FA$|^BAT32A279KM64FB$|^BAT32A279KM80FA$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/${vendor}/bat32a27/init_clock.c)
+endif()
